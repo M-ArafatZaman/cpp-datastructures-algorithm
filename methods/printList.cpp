5edc7529cc50fe34c49cf348513f6b7590c42580
Mohammad Arafat Zaman <mdarafatzaman1@gmail.com>
2020-11-23 18:41:29 +0600

    Added printList method

---
+++ methods/printList.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printList(int arr[], int size);
+void printList(vector<int> arr);
+
+int main() {
+
+    int a[] = {1,2,3,4,5};
+    vector<int> b = {6, 7,8,9,10};
+
+    printList(a, (sizeof(a))/(sizeof(*a)));
+    printList(b);
+}
+
+// Print integar array
+void printList(int arr[], int size) {
+    cout << "[";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << ",";
+    }
+    cout << "]" << endl;
+};
+
+// Print integar vector
+void printList(vector<int> arr) {
+    cout << "[";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << ",";
+    }
+    cout << "]" << endl;
+}
