d05b3f7d7ecbb8bc61e0dfd876d8a2e315852866
Mohammad Arafat Zaman <mdarafatzaman1@gmail.com>
2020-11-23 19:26:09 +0600

    Adding bubblesort algorithm

---
+++ algorithms/bubbleSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void swap_elements(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    return;
+}
+
+// A function which bubble sorts an array
+void bubbleSort(int arr[], int size) {
+    bool sorted = false;
+
+    do {
+        sorted = false;
+
+        for (int i = 0; i < size; i++) {
+            if ((arr[i] > arr[i+1]) && (i+1 < size)) {
+                swap_elements( arr[i], arr[i+1] );
+                sorted = true;
+            }
+        }
+
+    } while (sorted == true);
+
+}
+
+int main() {
+
+    int a[6] = {4,1,6,3,9,7};
+    cout << "Before sorting : ";
+    //printList(a, 5);
+    bubbleSort(a, 5); // usage
+    cout << "After sorting: ";
+    //printList(a, 5);
+    // printList(int a[], int size) is a function which prints an integart array
+
+}
+
