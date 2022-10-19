82310bdb1ae7acce9108d7a5385c70463d589682
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-10-19 06:55:49 +0600

    Added an insertion sort algorithm

---
+++ algorithms/insertionSort.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define DEBUG(x) cout << #x << " = " << x << endl;
+
+template <class T>
+void print_v(vector<T>& arr) {
+    cout << "{";
+    for (auto& i: arr) {
+        cout << i << ",";
+    };
+
+    cout << "}" << endl;
+    return;
+}
+
+// Function to swap pointer values
+template <class PointerClass>
+void swapPointerValues(PointerClass a, PointerClass b) {
+    auto temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+
+
+// Insertion sort function
+template <class IterClass>
+void insertionSort(IterClass begin, IterClass end) {
+    // Store the sorted part
+    IterClass sorted = begin + 1;
+    IterClass curr = sorted;
+
+    while (sorted < end) {
+        curr = sorted;
+
+        // Keep swapping until it is in the current position
+        while (*curr < *(curr-1) && curr > begin) {
+            swapPointerValues(curr, curr-1);
+            curr--;
+        }
+
+        sorted++;
+    }
+
+    return;
+}
+
+// Test runner exception
+class FailedTest: public exception {
+    public:
+        FailedTest(string err) {
+            cout << err << endl;
+        }
+};
+
+void test_runner() {
+
+    vector<int> a = {3,5,8,10,1,2,6,9,7};
+    vector<int> b = {1,2,3,5,6,7,8,9,10};
+    
+    insertionSort(a.begin(), a.end());
+    if (a != b) throw FailedTest("Quicksort could not properly sort the given array.");
+ 
+    cout << "All test passed!" << endl;
+
+};
+
+int main() {
+
+    test_runner();
+
+    return 0;
+}
